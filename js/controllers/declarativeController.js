29f5b074a90e299583680d252f2373b21fbd42dd
Juan Huerta <juan.huerta@gmail.com>
2016-04-25 11:13:27 +0200

    Directives as DSL, and using directives as comments

--- js/controllers/declarativeController.js
+++ js/controllers/declarativeController.js
@@ -5,39 +5,51 @@ var declarativeApp = angular.module('declarativeApp', []);
 /* */
 declarativeApp.controller('declarativeController', ['$scope', DeclarativeController]);
 
-function DeclarativeController(scope)
-{
-	scope.ItWorks = 'It works!';
+function DeclarativeController(scope) {
+    scope.ItWorks = 'It works!';
 
-	scope.student = {
-		Name: 'Juan',
-		Course: 'AngularJs'
-	};
+    scope.student = {
+        Name: 'Juan',
+        Course: 'AngularJs'
+    };
 
-	scope.edit = function(){
-		scope.editing = true;
-	};
-	scope.save = function(){
-		scope.editing = false;
-	};
+    scope.edit = function() {
+        scope.editing = true;
+    };
+    scope.save = function() {
+        scope.editing = false;
+    };
 }
 
 
-declarativeApp.controller('imperativeController', ['$scope', ImperativeController]);
 
-function ImperativeController(scope)
-{
-	scope.ItWorks = 'It works!';
 
-	scope.student = {
-		Name: 'Juan',
-		Course: 'AngularJs'
-	};
+declarativeApp.directive('afEditStudent', AFEditStudent);
 
-	scope.edit = function(){
-		scope.editing = true;
-	};
-	scope.save = function(){
-		scope.editing = false;
-	};
+function AFEditStudent() {
+    return {
+        restrict: 'E',
+        replace: true,
+        template: "<span ng-show='showThis'>" +
+            "<div>Name:<input ng-model='student.Name'></div>" +
+            "<div>Course:<input ng-model='student.Course'></div>" +
+            "<button ng-click=saveMethod()>Save</button>" +
+            "</span>",
+        scope: {
+            showThis: '=shows',
+            student: '=',
+            saveMethod: '=saveThisMethod'
+        }
+    };
+}
+
+declarativeApp.directive('afDisplayStudent', AFDisplayStudent);
+
+function AFDisplayStudent() {
+    return {
+        restrict: 'M',
+        replace: true,
+        template: "<span><div>Name:<span ng-bind='student.Name'></span></div>" +
+            "<div>Course:<span ng-bind='student.Course'></span></div></span>"
+    };
 }
